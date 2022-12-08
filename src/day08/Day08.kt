d922df8ae86bb8addef0572a8e583432770ecd47
robin schoch <rschoch1995@gmail.com>
2022-12-08 17:08:17 +0100

    # 7

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -17,22 +17,17 @@ class Forest(val trees: Array<IntArray>) {
     fun searchFromTop() = searchForVisibleTrees(trees[0].lastIndex downTo 0, trees.indices) { x, y -> y to x }
     fun searchFromBottom() = searchForVisibleTrees(trees[0].indices, trees.lastIndex downTo 0) { x, y -> y to x }
 
-    private fun searchForVisibleTrees(iRange: IntProgression, jRange: IntProgression, dir: (x: Int, y: Int) -> Pair<Int, Int>): MutableSet<Pair<Int, Int>> {
-        val visible = mutableSetOf<Pair<Int, Int>>()
-        iRange.forEach { i ->
-            var heightToBeat = -1
-            
-            jRange.forEach { j ->
-                dir(i, j).let {
-                    if (trees[it.first][it.second] > heightToBeat) {
-                        heightToBeat = trees[it.first][it.second]
-                        visible.add(it)
-                    }
+    private fun searchForVisibleTrees(iRange: IntProgression, jRange: IntProgression, dir: (x: Int, y: Int) -> Pair<Int, Int>) = iRange.flatMap { i ->
+        jRange.fold(-1 to setOf<Pair<Int, Int>>()) { acc, j ->
+            dir(i, j).let {
+                when (trees[it.first][it.second] > acc.first) {
+                    true -> trees[it.first][it.second] to (acc.second + it)
+                    false -> acc
                 }
             }
-        }
-        return visible
-    }
+        }.second
+    }.toSet()
+
 
     fun calculateScenicScore() = trees.flatMapIndexed { index, treeRow ->
         treeRow.mapIndexed { jndex, _ ->
@@ -71,14 +66,11 @@ object Day08 : AdventOfCodeSolution<Int, Int> {
 
     override val testSolution1 = 21
     override val testSolution2 = 8
-    override fun part1(input: List<String>) = with(Forest.seed(input).searchFromLeft()) {
-        addAll(Forest.seed(input).searchFromRight())
-        addAll(Forest.seed(input).searchFromTop())
-        addAll(Forest.seed(input).searchFromBottom())
-        size
-    }
+    override fun part1(input: List<String>) =
+        (Forest.seed(input).searchFromLeft() +
+            Forest.seed(input).searchFromRight() +
+            Forest.seed(input).searchFromTop() +
+            Forest.seed(input).searchFromBottom()).size
 
     override fun part2(input: List<String>) = Forest.seed(input).calculateScenicScore()
-
-
 }
